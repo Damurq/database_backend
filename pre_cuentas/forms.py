d03b3dbf08edd3d03af5ca56399e5f18ec37488e
Damurq <michaelmontero.idb@gmail.com>
2021-09-07 17:45:43 -0400

    models and form  V1

---
+++ pre_cuentas/forms.py
@@ -0,0 +1,46 @@
+from .models import *
+from django.forms import ModelForm
+
+
+class ClientForm(ModelForm):
+    # specify the name of model to use
+    class Meta:
+        model = Client
+        fields = ['document_type', 'document_number',
+                  'first_name', 'last_name', 'address']
+
+
+class ForeignTransferForm(ModelForm):
+    # specify the name of model to use
+    class Meta:
+        model = ForeignTransfer
+        fields = ['transfer_abroad', 'origin', 'destiny']
+
+
+class StateForm(ModelForm):
+    # specify the name of model to use
+    class Meta:
+        model = State
+        fields = ['name']
+
+
+class MunicipalityForm(ModelForm):
+    # specify the name of model to use
+    class Meta:
+        model = Municipality
+        fields = ['name', 'state_code']
+
+
+class OfficeForm(ModelForm):
+    # specify the name of model to use
+    class Meta:
+        model = Office
+        fields = ['name', 'municipality_code', 'address']
+
+
+class RequestForm(ModelForm):
+    # specify the name of model to use
+    class Meta:
+        model = Request
+        fields = ['office_code', 'foreign_transfer_code', 'account_type', 'reason', 'expiration_date', 'date_issue', 'account_usage', 'estimated_amount_mobilization', 'average_monthly_transaction',
+                  'background_source', 'background_destination']
--- pre_cuentas/models.py
+++ pre_cuentas/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Client(models.Model):
+    document_id = models.AutoField(primary_key=True)
+    document_type = models.CharField(max_length=1)                                  # Faltan opciones
+    document_number = models.PositiveIntegerField(max_length=10)                    # Faltan opciones
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.TextField(max_length=500)
+    state = models.CharField(max_length=1)                                          # Faltan opciones
+    def __str__(self):
+        return self.document_id
+    class Meta:  
+        db_table = 'Client'
+        constraints = [
+            models.UniqueConstraint(fields=['document_type', 'document_number'], name='unique_document')
+        ]
+
+class ForeignTransfer(models.Model):
+    transfer_abroad = models.CharField(max_length=1)
+    origin = models.CharField(max_length=1, blank=True, null=True)
+    destiny = models.CharField(max_length=1, blank=True, null=True)
+    def __str__(self):
+        return self.name
+    class Meta:  
+        db_table = 'ForeignTransfer'
+
+class State(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    state = models.CharField(max_length=1)                                          # Faltan opciones
+    def __str__(self):
+        return self.name
+    class Meta:  
+        db_table = 'State'
+
+class Municipality(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    state_code = models.ForeignKey(State, on_delete= models.CASCADE)
+    state = models.CharField(max_length=1)                                          # Faltan opciones
+    def __str__(self):
+        return self.name
+    class Meta:  
+        db_table = 'Municipality'
+
+class Office(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    municipality_code = models.ForeignKey(Municipality, on_delete= models.CASCADE)
+    address = models.TextField(max_length=500)
+    state = models.CharField(max_length=1)                                          # Faltan opciones
+    def __str__(self):
+        return self.name
+    class Meta:  
+        db_table = 'Office'
+
+class Request(models.Model):
+    code = models.AutoField(primery_key=True)
+    client_document_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    office_code = models.ForeignKey(Office, on_delete=models.CASCADE)
+    foreign_transfer_code = models.ForeignKey(Office, on_delete=models.PROTECT)
+    account_type = models.CharField(max_length=1)                                   # Faltan opciones
+    reason = models.CharField(max_length=1)                                         # Faltan opciones
+    expiration_date = models.DateField(auto_now =False, auto_now_add=False)
+    date_issue = models.DateField(auto_now =False, auto_now_add=True)
+    account_usage = models.CharField(max_length=1)
+    estimated_amount_mobilization = models.CharField(max_length=1)                  # Faltan opciones
+    average_monthly_transaction = models.CharField(max_length=1)                    # Faltan opciones
+    background_source = models.CharField(max_length=1)                              # Faltan opciones
+    background_destination = models.CharField(max_length=1)                         # Faltan opciones
+    state = models.CharField(max_length=1)                                          # Faltan opciones
+    def __str__(self):
+        return self.code
+    class Meta:  
+        db_table = 'Request'
+
