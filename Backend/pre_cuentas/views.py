b70f4b5cb1bf168506aaa2a682ee87b40561fe7e
Damurq <michaelmontero.idb@gmail.com>
2021-09-12 14:51:47 -0400

    pip file and migrations

--- Backend/pre_cuentas/views.py
+++ Backend/pre_cuentas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import *
+  
+def client_view(request):
+    context ={}
+    # create object of form
+    form = ClientForm(request.POST or None, request.FILES or None)
+    # check if form data is valid
+    if form.is_valid():
+        # save the form data to model
+        form.save()
+    context['form']= form
+    return render(request, "home.html", context)
