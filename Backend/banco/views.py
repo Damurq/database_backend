a90be5729cab3fbf5fa8bfaa7fab8fcaa79f86a9
Mari <marihecmiranda160498@gmail.com>
2021-09-13 20:29:15 -0400

    Vistas login-seeQuote-visualize y algunas modificaciones

--- Backend/banco/views.py
+++ Backend/banco/views.py
@@ -1,4 +1,32 @@
+from os import name
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
+
+def Login(request):
+    return render(request, 'pages/Login.html')
+
+def homeUser(request):
+    return render(request, 'pages/homeUser.html')
+
+def HomeUser(request):
+    return render(request, 'HomeUser.html')
+
+def MessageSC(request):
+    return render(request, 'components/MessageSC.html')
+
+def ClientDataSC(request):
+    return render(request, 'components/ClientDataSC.html')
+
+def Form1SC(request):
+    return render(request, 'components/Form1SC.html')
+
+def Form2SC(request):
+    return render(request, 'components/Form2SC.html')
+
+def SeeQuote(request):
+    return render(request, 'components/SeeQuote.html')
+
+def Visualize(request):
+    return render(request, 'components/Visualize.html')
+
 
--- Backend/pre_cuentas/views.py
+++ Backend/pre_cuentas/views.py
@@ -1,19 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
-#@login_required
-def homeUser(request):
-    return render(request, 'HomeUser.html')
-
-def messageSC(request):
-    return render(request, 'components/MessageSC.html')
-
-def clientDataSC(request):
-    return render(request, 'components/ClientDataSC.html')
-
-def form1SC(request):
-    return render(request, 'components/Form1SC.html')
-
-def form2SC(request):
-    return render(request, 'components/Form2SC.html')
+from .forms import *
+  
+def client_view(request):
+    context ={}
+    # create object of form
+    form = ClientForm(request.POST)
+    # check if form data is valid
+    if form.is_valid():
+        # save the form data to model
+        form.save()
+    context['form']= form
+    return render(request, "pages/cliente.html", context)
