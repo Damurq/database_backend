5b04dbf4461ea59b93bf6f5c6ab91ef8467e5254
Damurq <michaelmontero.idb@gmail.com>
2021-09-15 21:42:22 -0400

    models in admin

--- Backend/pre_cuentas/admin.py
+++ Backend/pre_cuentas/admin.py
@@ -4,4 +4,29 @@ from .models import *
 class ClientAdmin(admin.ModelAdmin):
     pass
 
+class ForeignTransferAdmin(admin.ModelAdmin):
+    pass
+
+
+class StateAdmin(admin.ModelAdmin):
+    pass
+
+
+class MunicipalityAdmin(admin.ModelAdmin):
+    pass
+
+
+class OfficeAdmin(admin.ModelAdmin):
+    pass
+
+
+class RequestAdmin(admin.ModelAdmin):    
+    pass
+
+
 admin.site.register(Client, ClientAdmin)
+admin.site.register(ForeignTransfer, ForeignTransferAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(Municipality, MunicipalityAdmin)
+admin.site.register(Office, OfficeAdmin)
+admin.site.register(Request, RequestAdmin)
